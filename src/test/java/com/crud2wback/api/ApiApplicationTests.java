c8575ccea08b6036f28bebc6f0c53a9c2edfbb99
Masih Saldanha Xavier <masih.saldanha@gmail.com>
2024-06-17 17:46:30 -0300

    chore: first commit

---
+++ src/test/java/com/crud2wback/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud2wback.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
